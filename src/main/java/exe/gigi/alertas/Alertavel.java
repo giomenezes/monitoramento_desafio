2ca10cc825779c23291ac66f39c47def1be336b2
giomenezes <giovanna.rodrigues@sptech.school>
2023-12-11 13:36:35 -0300

    cleanup(): limpeza

--- src/main/java/exe/gigi/alertas/Alertavel.java
+++
@@ -1,6 +0,0 @@
-package exe.gigi.alertas;
-
-public interface Alertavel {
-    Integer alertarAtencao();
-    Integer alertarUrgencia();
-}
--- src/main/java/exe/gigi/alertas/Config.java
+++
@@ -1,8 +0,0 @@
-package exe.gigi.alertas;
-
-public class Config {
-    private static String token = "xoxe.xoxp-1-Mi0yLTU3OTIzOTkyOTMyODEtNTk2NDgxMDAzMDczNy02MzMzNDQzOTA3NDA5LTYzMDYyNzE0MTQxNjctZjhlNGVlMGM5NjJhOTRmNjdjZjRiNTUxNmNlN2QzNjlkN2UwZTJiNjEzZDhjZWIyZWYyODdmMGY5OGEyOWEwNQ";
-    public static String getToken() {
-        return token;
-    }
-}
